94663d98c148b4927ce2dafe79c7786bbb4e538c
노영주 <at8in@naver.com>
2021-07-25 05:12:34 +0900

    feat: add card and edit form component

--- JavaScript/React/react-basic/card-maker/src/components/maker/maker.jsx
+++ JavaScript/React/react-basic/card-maker/src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -6,6 +6,41 @@ import Header from '../header/header';
 import Preview from '../preview/preview';
 import styles from './maker.module.css';
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+       {
+           id: '1',
+           name: 'Young Ju',
+           company: 'Good Company',
+           theme: 'dark',
+           title: 'Software Engineer',
+           email: 'younggu@gmail.com',
+           message: 'go for it',
+           fileName: 'young',
+           fileURL:null
+        } ,
+        {
+            id: '2',
+            name: 'Nyan Dol',
+            company: 'My heart',
+            theme: 'colorful',
+            title: 'Cute Cat',
+            email: 'catheaven@gmail.com',
+            message: 'Love!',
+            fileName: 'nyandol',
+            fileURL:null
+        } ,
+        {
+            id: '3',
+            name: 'AKIRA',
+            company: 'OTOMO',
+            theme: 'light',
+            title: 'Animation',
+            email: 'akira@gmail.com',
+            message: 'regend',
+            fileName: 'akira',
+            fileURL:null
+        } 
+    ]);
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -22,8 +57,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards}/>
             </div>
             <Footer />
         </section>
