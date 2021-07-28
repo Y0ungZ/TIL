1e1b9493acfe57645fa28a596868e791ef1e2a9d
노영주 <at8in@naver.com>
2021-07-28 23:32:56 +0900

    [백준][JavaScript][1000]A+B

---
+++ JavaScript/Problem-Solving/백준/JavaScript/1000_A+B.js
@@ -0,0 +1,10 @@
+/*
+https://www.acmicpc.net/problem/1000
+*/
+"use strict";
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+console.log(a + b);
