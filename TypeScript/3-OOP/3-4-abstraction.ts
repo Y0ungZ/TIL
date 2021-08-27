b671180182066a2eef8b38991281863f25c883da
노영주 <at8in@naver.com>
2021-08-27 16:44:33 +0900

    [TS]OOP: 추상화

---
+++ TypeScript/3-OOP/3-4-abstraction.ts
@@ -0,0 +1,94 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  //계약서. 외부적으로 사용되는 것이기때문에 최대한 명확하고 깔끔하게 작성
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
+  //interface를 implement.
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; //변하지 않는 값 -> static(class level)
+    private coffeeBeans: number = 0; //instance(object) level
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    } //권장
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log("cleaning the machine...");
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up... 🔥");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots... ☕`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    //interface를 implements하여 꼭 구현해야 하는 함수
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(45);
+      this.machine.clean();
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  const amateur = new AmateurUser(maker);
+  const pro = new ProBarista(maker);
+  //   amateur.makeCoffee();
+  pro.makeCoffee();
+}
