b84a6e5163fe1f29a8da2c7ff91c1f5bf81e24f0
노영주 <at8in@naver.com>
2021-12-01 23:43:44 +0900

    [PRO][JavaScript]다트게임

---
+++ Problem-Solving/프로그래머스/JavaScript/다트게임.js
@@ -0,0 +1,48 @@
+const solution = (dartResult) => {
+  let score = dartResult.match(/10|[0-9]/g);
+  let dart = dartResult.split(/10|[0-9]/).slice(1);
+  console.log(score);
+  console.log(dart);
+  let answer = 0;
+  let num = 0;
+
+  for (let i = 0; i < 3; i++) {
+    if (dart[i].length === 2) {
+      let bonus = dart[i].charAt(0);
+      let option = dart[i].charAt(1);
+
+      if (bonus === 'D') {
+        score[i] = Math.pow(score[i], 2);
+      } else if (bonus === 'T') {
+        score[i] = Math.pow(score[i], 3);
+      }
+
+      if (option === '*') {
+        if (i === 0) {
+          score[i] *= 2;
+        } else {
+          score[i] *= 2;
+          score[i - 1] *= 2;
+        }
+      } else {
+        if (i === 0) {
+          score[i] *= -1;
+        } else {
+          score[i] *= -1;
+          score[i - 1] *= -1;
+        }
+      }
+    } else {
+      let bonus = dart[i].charAt(0);
+      if (bonus === 'D') {
+        score[i] = Math.pow(score[i], 2);
+      } else if (bonus === 'T') {
+        score[i] = Math.pow(score[i], 3);
+      }
+    }
+  }
+
+  console.log(score);
+
+  return answer;
+};
