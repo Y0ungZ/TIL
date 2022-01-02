509780605b7272e58ab19bfd969238cd90ca2ad2
at8in <at8in@naver.com>
2022-01-02 23:35:31 +0900

    [JS]모던자바스크립트 과제6 - 비교연산자

---
+++ JavaScript/modernJS/6_comparison/assignment1.js
@@ -0,0 +1,7 @@
+5 > 4; // true
+'apple' > 'pineapple'; // false
+'2' > '12'; // true, 문자열끼리는 사전순
+undefined == null; // true
+undefined === null; // false(형(types)이 다름)
+null == '\n0\n'; // false(null은 오직 undefined와 같다.)
+null === +'\n0\n'; // false
