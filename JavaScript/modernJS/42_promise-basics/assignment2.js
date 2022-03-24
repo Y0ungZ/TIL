e7ce556ec5d1ce9f88f751c74a05e683924a912f
at8in <at8in@naver.com>
2022-03-25 02:52:13 +0900

    [JS]모던자바스크립트 과제42 - 프라미스

---
+++ JavaScript/modernJS/42_promise-basics/assignment2.js
@@ -0,0 +1,8 @@
+function delay(ms) {
+  // 여기에 코드 작성
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(), ms);
+  });
+}
+
+delay(3000).then(() => console.log('3초후 실행'));
