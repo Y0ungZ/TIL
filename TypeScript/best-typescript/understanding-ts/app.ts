f3d304b82906726000b3fa9c92bf97b1e365a0f4
at8in <at8in@naver.com>
2022-05-25 18:03:54 +0900

    [TS] Udemy - boolean, string

--- TypeScript/best-typescript/understanding-ts/app.ts
+++ TypeScript/best-typescript/understanding-ts/app.ts
@@ -1,9 +1,15 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(n1: number, n2: number, showResult : boolean, phrase:string) {
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);        
+    } else {
+        return result;
+    }
 }
 
 const number1 = 5; // surely number
 const number2 = 2.8; // surely number
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2,printResult, resultPhrase);
