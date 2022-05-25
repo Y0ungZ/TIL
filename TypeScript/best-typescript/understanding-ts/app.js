081b86b5330730273504a3ca32e99dc92e82dbcb
at8in <at8in@naver.com>
2022-05-26 02:29:38 +0900

    [TS] Udemy - object

--- TypeScript/best-typescript/understanding-ts/app.js
+++ TypeScript/best-typescript/understanding-ts/app.js
@@ -1,14 +1,14 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5; // surely number
-var number2 = 2.8; // surely number
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
+//(1)
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+var person = {
+    name: 'YoungJu',
+    age: 100
+};
+//person:object => 포괄적. person.name을 할 경우 컴파일 오류.
+//WHY? object를 타입스크립트는 어떤 정보도 주지 않는 객체가 있다고 이해한다.
+//객체에 대한 정보, 어떤 타입의 속성도 지원하지 않음.
+//다만, (1)처럼 명시적으로 지정하는 것은 좋은 방식이 아니다.
+console.log(person.name);
--- TypeScript/best-typescript/understanding-ts/app.ts
+++ TypeScript/best-typescript/understanding-ts/app.ts
@@ -1,15 +1,17 @@
-function add(n1: number, n2: number, showResult : boolean, phrase:string) {
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);        
-    } else {
-        return result;
-    }
-}
+//(1)
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
+  name: 'YoungJu',
+  age: 100,
+};
 
-const number1 = 5; // surely number
-const number2 = 2.8; // surely number
-const printResult = true;
-const resultPhrase = "Result is: ";
+//person:object => 포괄적. person.name을 할 경우 컴파일 오류.
+//WHY? object를 타입스크립트는 어떤 정보도 주지 않는 객체가 있다고 이해한다.
+//객체에 대한 정보, 어떤 타입의 속성도 지원하지 않음.
 
-add(number1, number2,printResult, resultPhrase);
+//다만, (1)처럼 명시적으로 지정하는 것은 좋은 방식이 아니다.
+
+console.log(person.name);
