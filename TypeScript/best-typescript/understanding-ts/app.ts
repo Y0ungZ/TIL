32e7bf9b073d69e907401a9b75d55f42e9f49c7a
at8in <at8in@naver.com>
2022-05-25 17:59:12 +0900

    [TS] Udemy - Number

---
+++ TypeScript/best-typescript/understanding-ts/app.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const number1 = 5; // surely number
+const number2 = 2.8; // surely number
+
+const result = add(number1, number2);
+console.log(result);
