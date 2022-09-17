857301d455a5b1bf1a0396a4b8cd1674b8bd86e8
at8in <at8in@naver.com>
2022-09-17 16:44:00 +0900

    [PRO][JavaScript][R]튜플

--- Problem-Solving/프로그래머스/JavaScript/튜플.js
+++ Problem-Solving/프로그래머스/JavaScript/튜플.js
@@ -1,46 +1,14 @@
 function solution(s) {
-  const answer = [];
-  const union = [];
+  const answer = new Set();
+  const array = JSON.parse(s.replace(/{/g, "[").replace(/}/g, "]")).sort(
+    (a, b) => a.length - b.length
+  );
 
-  let temp = [];
-  let str = '';
+  array.forEach((row) => {
+    row.forEach((element) => {
+      answer.add(element);
+    });
+  });
 
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === '{') {
-      temp = [];
-      str = '';
-    } else if (s[i] === '}') {
-      if (str.length) {
-        temp.push(+str);
-      }
-      if (temp.length) {
-        union.push(temp);
-      }
-
-      temp = [];
-      str = '';
-    } else if (s[i] === ',') {
-      temp.push(+str);
-      str = '';
-    } else {
-      str += s[i];
-    }
-  }
-
-  union.sort((a, b) => a.length - b.length);
-
-  const set = new Set();
-
-  for (let i = 0; i < union.length; i++) {
-    const current = union[i];
-
-    for (let j = 0; j < current.length; j++) {
-      if (!set.has(current[j])) {
-        set.add(current[j]);
-        answer.push(current[j]);
-      }
-    }
-  }
-
-  return answer;
+  return Array.from(answer);
 }
