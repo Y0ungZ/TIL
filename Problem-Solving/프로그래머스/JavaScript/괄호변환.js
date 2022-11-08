3f00c112bd1ac789f68ac78398ef67dc2e0e62fc
at8in <at8in@naver.com>
2022-11-08 13:25:01 +0900

    [PRO][JavaScript][R]괄호변환

--- Problem-Solving/프로그래머스/JavaScript/괄호변환.js
+++ Problem-Solving/프로그래머스/JavaScript/괄호변환.js
@@ -1,75 +1,71 @@
-const isCorrect = (u) => {
-  let openBracket = 0;
-  let closeBracket = 0;
+function solution(p) {
+  if (isCorrectString(p)) {
+    return p;
+  }
 
-  for (let i = 0; i < u.length; i++) {
-    if (u[i] === '(') {
-      openBracket++;
+  return divideString(p);
+}
+
+/**
+ * 주어진 "과정"을 수행하는 재귀함수.
+ * @returns {string}
+ */
+function divideString(str) {
+  // 1. 빈 문자열일 경우, 그대로 반환.
+  if (str === "") return str;
+
+  // 2. 두 균형잡힌 괄호 문자열 u, v 로 분리한다.
+  const balancedIndex = getBalancedIndex(str);
+  const u = str.slice(0, balancedIndex + 1);
+  const v = str.slice(balancedIndex + 1);
+
+  if (isCorrectString(u)) {
+    // 3. u가 올바른 괄호 문자열이라면 v에 대해 1단계부터 수행, 이어 붙여 반환.
+    return u + divideString(v);
+  }
+
+  // 4. u가 올바른 괄호 문자열이 아니다.
+  // 4-4.
+  let reverseU = "";
+
+  for (let index = 1; index < u.length - 1; index++) {
+    reverseU += u[index] === "(" ? ")" : "(";
+  }
+
+  // 4-1,2,3,5
+  return "(" + divideString(v) + ")" + reverseU;
+}
+
+/**
+ * 균형잡힌 괄호 문자열을 만드는 Index 값을 리턴한다.
+ * @returns {number}
+ */
+function getBalancedIndex(str) {
+  let leftBracketCnt = 0;
+  let rightBracketCnt = 0;
+
+  for (let index = 0; index < str.length; index++) {
+    str[index] === "(" ? leftBracketCnt++ : rightBracketCnt++;
+
+    if (leftBracketCnt === rightBracketCnt) return index;
+  }
+}
+
+/**
+ * 올바른 괄호 문자열인지 판별한다.
+ * @returns {boolean}
+ */
+function isCorrectString(str) {
+  const stack = [];
+  for (const s of str) {
+    if (s === "(") {
+      stack.push("(");
     } else {
-      closeBracket++;
-      if (closeBracket > openBracket) {
+      const lastInput = stack.pop();
+      if (lastInput !== "(") {
         return false;
       }
     }
   }
   return true;
-};
-
-const isBalance = (u) => {
-  let openBracket = 0;
-  let closeBracket = 0;
-  let sliceIdx = 0;
-
-  for (let i = 0; i < u.length; i++) {
-    if (u[i] === '(') {
-      openBracket++;
-    } else {
-      closeBracket++;
-    }
-
-    if (openBracket === closeBracket) {
-      sliceIdx = i;
-      break;
-    }
-  }
-
-  return sliceIdx;
-};
-
-const changeBracket = (w) => {
-  if (!w.length) {
-    return '';
-  } else {
-    let sliceIdx = isBalance(w);
-    let u = w.slice(0, sliceIdx + 1);
-    let v = w.slice(sliceIdx + 1, w.length);
-
-    if (isCorrect(u)) {
-      return u + changeBracket(v);
-    } else {
-      let temp = changeBracket(v);
-      let uSlice = '';
-
-      for (let i = 1; i < u.length - 1; i++) {
-        if (u[i] === '(') {
-          uSlice += ')';
-        } else {
-          uSlice += '(';
-        }
-      }
-
-      return '(' + temp + ')' + uSlice;
-    }
-  }
-};
-
-const solution = (p) => {
-  let answer = '';
-
-  if (isCorrect(p)) {
-    answer = p;
-  } else {
-    answer = changeBracket(p);
-  }
-  return answer;
-};
+}
