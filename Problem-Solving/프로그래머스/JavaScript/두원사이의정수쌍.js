08fad3b8edcee12c58ade1a4e3821a6500cb1415
Y0ungZ <at8in@naver.com>
2024-08-13 15:44:04 +0900

    [PRO][JavaScript]두 원 사이의 정수 쌍

---
+++ Problem-Solving/프로그래머스/JavaScript/두원사이의정수쌍.js
@@ -0,0 +1,16 @@
+function solution(r1, r2) {
+  let answer = 0;
+
+  for (let x = 1; x <= r2; x++) {
+    let y1 = Math.ceil(Math.sqrt(Math.pow(r1, 2) - Math.pow(x, 2)));
+    const y2 = Math.floor(Math.sqrt(Math.pow(r2, 2) - Math.pow(x, 2)));
+
+    if (Number.isNaN(y1)) {
+      y1 = 0;
+    }
+
+    answer += y2 - y1 + 1;
+  }
+
+  return answer * 4;
+}
